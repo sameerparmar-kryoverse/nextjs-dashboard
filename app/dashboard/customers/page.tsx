39986068e0841e8f79d5f8df41c2949028474254
sameerparmar-kryoverse <sameer@kryoverse.com>
2024-03-15 12:50:11 +0530

    Complate remaining functionality

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,26 @@
-export default function Page() {
-    return <p>Customers Page</p>;
+import Table from "@/app/ui/customers/table";
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: 'InvoiCustomersces',
+  };
+  
+  
+  export default async function Page({ searchParams } : {
+      searchParams?: {
+          query?: string,
+          page?: string,
+        };
+    }) {
+    
+    const query = searchParams?.query || '';
+  
+    const customers = await fetchFilteredCustomers(query);
+        
+  return (
+    <div className="w-full">
+        <Table customers={customers} />
+    </div>
+  );
 }
